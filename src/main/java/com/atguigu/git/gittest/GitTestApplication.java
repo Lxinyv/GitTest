d48b839cfa11a34c9a506215304bcff6a09412af
qqLxinyvqq <116242150+qqLxinyvqq@users.noreply.github.com>
2022-10-26 10:43:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gittest/GitTestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.git.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+      System.out.println("first commit");
+    }
+
+}
