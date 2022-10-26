be06592fcbff0ff51da9c5eb954c85fe9d42dc83
qqLxinyvqq <116242150+qqLxinyvqq@users.noreply.github.com>
2022-10-26 10:46:27 +0800

    third commit

--- src/main/java/com/atguigu/git/gittest/GitTestApplication.java
+++ src/main/java/com/atguigu/git/gittest/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
       System.out.println("first commit");
       System.out.println("second commit");
+      System.out.println("third commit");
     }
 
 }
